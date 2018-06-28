c0de976db93e197a5b882f468cfee0eba1f79a78
lukekorsman <korsmanl@seattleu.edu>
2018-06-27 20:37:08 -0700

    First commit

---
+++ EWWebApp/EWWebApp/Models/EncryptWordModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace EWWebApp.Models
+{
+    public class EncryptWordModel
+    {
+        [Display(Name = "Word", Description = "Word to encrypt")]
+        [Required(ErrorMessage = "Word is required")]
+        public string Word { get; set; }
+    }
+}
